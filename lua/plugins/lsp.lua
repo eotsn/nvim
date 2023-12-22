1da28424701574533f62c564ceab20036aa7d694
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2023-12-22 09:47:45 +0100

    Enable automatic LSP server installation
    Who cares about NixOS anyway?

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,8 +56,12 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-      -- Ensure that the servers are configured
+      -- Ensure that the servers are installed
       local mason_lspconfig = require 'mason-lspconfig'
+
+      mason_lspconfig.setup {
+        ensure_installed = vim.tbl_keys(servers),
+      }
 
       mason_lspconfig.setup_handlers {
         function(server_name)
