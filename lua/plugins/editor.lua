6d85151f0c5d55c273c4d6e3462e253920f3b231
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-01-28 22:54:07 +0100

    feat(editor): buffer navigation with Harpoon

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -87,6 +87,25 @@ return {
     },
   },
 
+  -- Simple, project-based navigation for a specific set of buffers
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup({})
+
+      vim.keymap.set("n", "<leader>A", function() harpoon:list():append() end, { desc = "Harpoon file" })
+      vim.keymap.set("n", "<leader>a", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon quick menu" })
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon to file 1" })
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon to file 2" })
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon to file 3" })
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon to file 4" })
+      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon to file 5" })
+    end,
+  },
+
   -- Obfuscate matched patterns in defined filetypes
   { "laytan/cloak.nvim", config = true },
 }
