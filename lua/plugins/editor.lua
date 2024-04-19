0194db6e111a4407ed8ecfcf65c7884fe6f358d3
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-04-19 13:58:59 +0200

    style: add stylua config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -99,6 +99,7 @@ return {
       local harpoon = require("harpoon")
       harpoon:setup({})
 
+      -- stylua: ignore start
       vim.keymap.set("n", "<leader>A", function() harpoon:list():append() end, { desc = "Harpoon file" })
       vim.keymap.set("n", "<leader>a", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon quick menu" })
       vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon to file 1" })
@@ -106,6 +107,7 @@ return {
       vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon to file 3" })
       vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon to file 4" })
       vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon to file 5" })
+      -- stylua: ignore end
     end,
   },
 
