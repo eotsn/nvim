774663a2d9fb53a05fc029d66ef28bbc55f313fe
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-05-03 22:31:01 +0200

    feat(telescope): remove devicons

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -7,7 +7,6 @@ return {
     branch = "0.1.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = is_windows
