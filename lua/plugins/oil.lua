96e7104f0e580f9f948b3d476958a2d25c403c38
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-06-06 16:06:19 +0200

    feat(oil): show additional file metadata

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,14 @@
 return {
   "stevearc/oil.nvim",
   config = function()
-    require("oil").setup()
+    require("oil").setup {
+      columns = {
+        "permissions",
+        "size",
+        "mtime",
+        "icon",
+      },
+    }
 
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
