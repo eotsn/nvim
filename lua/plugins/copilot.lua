beb15453cf82bc6c64fa2dde3207ec10b5fbf13e
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-06-11 23:02:17 +0200

    feat(copilot): keep Copilot disabled by default

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
   "github/copilot.vim",
+  config = function()
+    -- Keep Copilot disabled by default
+    vim.g.copilot_enabled = 0
+
+    vim.keymap.set("n", "<leader>cc", function()
+      if vim.g.copilot_enabled ~= 0 then
+        vim.cmd.Copilot { "disable" }
+      else
+        vim.cmd.Copilot { "enable" }
+      end
+    end, { noremap = true, silent = true })
+  end,
 }
