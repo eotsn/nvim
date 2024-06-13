a7a5ec14201c865af374740cd23592001059d474
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-06-13 17:16:40 +0200

    feat(colorscheme): always use Modus Operandi

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("modus-themes").setup {
+      style = "modus_operandi",
       on_highlights = function(highlights, colors)
         highlights.Whitespace = { fg = colors.bg_dim }
       end,
