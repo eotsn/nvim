43a6d6561df24d87bcc7dc9ab80b62d02194b7f7
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-07-06 21:46:13 +0200

    feat(lsp): enable LSP inlay hints and codelenses

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,39 @@ return {
     },
     opts = {
       servers = {
-        lua_ls = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              codelens = {
+                enable = true,
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+              },
+            },
+          },
+        },
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              staticcheck = true,
+            },
+          },
+        },
       },
     },
     config = function(_, opts)
@@ -46,8 +78,29 @@ return {
           vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = bufnr })
           vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = bufnr })
 
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+          vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, { buffer = bufnr })
+
           vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = bufnr })
           vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+          vim.keymap.set("n", "<space>cc", vim.lsp.codelens.run, { buffer = bufnr })
+
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+            vim.keymap.set("n", "<space>ch", function()
+              ---@diagnostic disable-next-line: missing-parameter
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            end)
+          end
+
+          if client and client.server_capabilities.codeLensProvider and vim.lsp.codelens then
+            vim.lsp.codelens.refresh()
+            vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+              buffer = bufnr,
+              callback = vim.lsp.codelens.refresh,
+            })
+          end
         end,
       })
     end,
