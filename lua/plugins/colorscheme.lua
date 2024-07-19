52e11343f7f0c76d5ebaf384d3719d9436528984
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-07-20 01:17:00 +0200

    feat(colorscheme): update colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,21 +1,10 @@
 return {
   {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "tokyobones"
-
-      local lush = require "lush"
-      local base = require "tokyobones"
-      local specs = lush.parse(function()
-        return {
-          ---@diagnostic disable-next-line: undefined-global
-          ColorColumn { base.CursorLine },
-        }
-      end)
-      lush.apply(lush.compile(specs))
+      vim.cmd.colorscheme "tokyonight"
     end,
   },
 }
