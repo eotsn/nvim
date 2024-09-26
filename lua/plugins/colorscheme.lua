3dbcc030ada4653edc326ddc542b86c7fd32535f
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-09-26 22:30:36 +0200

    Disable @comment.todo accent color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,15 @@ return {
   "EdenEast/nightfox.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    groups = {
+      all = {
+        ["@comment.todo"] = { link = "Comment" },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nightfox").setup(opts)
     vim.cmd [[colorscheme dayfox]]
   end,
 }
